067458d6c841d5b4a4fbfaca351dd3c38f52606d
Artem <artemmail@gmail.com>
2024-10-22 18:02:30 +0400

    commit9

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
-import Button from './components/Button/Button';
 import Header from './components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 function App() {
   const {onToggleButton, tg} = useTelegram()
@@ -14,7 +16,10 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Button onClick={onToggleButton}>pfr</Button>
+      <Routes>
+        <Route index element={<ProductList/>}/>
+        <Route path={'form'} element={<Form/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './Form.css'
+
+const Form = () => {
+    count [country, setCountry] = useState('')
+    count [street, setStreet] = useState('')
+    count [subject, setSubject] = useState('physical')
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+    return (
+        <div className={'form'}>
+            <h3>Введите ваши данные</h3>
+            <input 
+                className={'input'} 
+                type='text' 
+                placeholder={'Страна'}
+                value={country}
+                onChange={() => onChangeCountry}
+            />
+            <input 
+                className={'input'} 
+                type='text' 
+                placeholder={'Улица '}
+                value={street}
+                onChange={() => onChangeStreet}
+            />
+            <select 
+                value={subject} 
+                onChange={() => onChangeSubject} 
+                className={'select'}
+            >
+                <option value={'physical'}>Физическое лицо</option>
+                <option value={'legal'}>Юридическое лицо</option>
+            </select>
+        </div>
+        
+    );
+};
+
+export default Form;
