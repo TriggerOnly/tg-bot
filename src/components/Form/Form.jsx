899736cd7787ab45d275e8b5010b8182ae6b93d3
TriggerOnly <127595188+TriggerOnly@users.noreply.github.com>
2024-10-22 20:15:10 +0400

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,38 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import './Form.css'
+import './Form.css';
 import { useTelegram } from '../../hooks/useTelegram';
 
 const Form = () => {
-    const [country, setCountry] = useState('')
-    const [street, setStreet] = useState('')
-    const [subject, setSubject] = useState('physical')
-    const tg = useTelegram()
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const tg = useTelegram();
 
     useEffect(() => {
-        tg.MainButton.setParams({
-            text: 'Отправить данные'
-        })
-    }, [])
-
-    useEffect(() => {
-        if(!country || !street) {
-            tg.MainButton.hide()
-        } else {
-            tg.MainButton.show()
+        if (tg.MainButton) { // Проверка на наличие tg.MainButton
+            tg.MainButton.setParams({
+                text: 'Отправить данные',
+            });
         }
-    }, [country, street])
+    }, [tg]);
+
+    useEffect(() => {
+        if (!country || !street) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, street, tg]); // Добавление tg в зависимости
 
     const onChangeCountry = (e) => {
-        setCountry(e.target.value)
-    }
+        setCountry(e.target.value);
+    };
 
     const onChangeStreet = (e) => {
-        setStreet(e.target.value)
-    }
+        setStreet(e.target.value);
+    };
 
     const onChangeSubject = (e) => {
-        setSubject(e.target.value)
-    }
+        setSubject(e.target.value);
+    };
 
     return (
         <div className={'form'}>
@@ -42,25 +44,24 @@ const Form = () => {
                 type='text' 
                 placeholder={'Страна'}
                 value={country}
-                onChange={() => onChangeCountry}
+                onChange={onChangeCountry} // Исправлено
             />
             <input 
                 className={'input'} 
                 type='text' 
                 placeholder={'Улица '}
                 value={street}
-                onChange={() => onChangeStreet}
+                onChange={onChangeStreet} // Исправлено
             />
             <select 
                 value={subject} 
-                onChange={() => onChangeSubject} 
+                onChange={onChangeSubject} // Исправлено
                 className={'select'}
             >
                 <option value={'physical'}>Физическое лицо</option>
                 <option value={'legal'}>Юридическое лицо</option>
             </select>
         </div>
-        
     );
 };
 
